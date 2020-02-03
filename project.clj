5eec10357aefc6d76e54a356bcc6ba8ffbd21d9d
Will Acton <will@amperity.com>
2020-02-03 12:31:47 -0800

    0.0.7

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject lilactown/helix "0.0.6"
+(defproject lilactown/helix "0.0.7"
   :description "ClojureScript optimized for modern React development."
   :url "https://github.com/Lokeh/helix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :source-paths ["src"]
-  :dependencies [[ilk "1.0.0"]
-                 [cljs-bean "1.5.0"]]
+  :dependencies [[cljs-bean "1.5.0"]]
   :deploy-repositories [["snapshots" {:sign-releases false :url "https://clojars.org"}]])
