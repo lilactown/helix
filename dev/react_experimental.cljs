84b65fe0db74a4e528ded8af84056c28adf524ad
Will Acton <will@willacton.com>
2020-02-07 19:58:16 -0800

    suspense example

--- dev/react_experimental.cljs
+++ dev/react_experimental.cljs
@@ -9,17 +9,94 @@
    [react-experimental :refer [defnc]]))
 
 
+(declare fetch-data)
+
+
+(declare read)
+
+
+;;
+;; App stuff
+;;
+
+
+(def data (atom nil))
+
+
+(defnc data-view
+  []
+  (let [data (read @data)]
+    (d/div
+     (pr-str data))))
+
+
 (defnc app
   []
-  "asdfed")
+  (suspense
+   {:fallback (d/h1 "Loading data...")}
+   ($ data-view)
+   ($ data-view)
+   ($ data-view)
+   ($ data-view)
+   ($ data-view)))
 
 
-(defn start []
+(defn start
+  []
   (refresh/inject-hook!)
   (-> (js/document.getElementById "app")
       (rdom/createRoot)
       (.render ($ app))))
 
 
-(defn ^:dev/after-load reload []
+(defn ^:dev/after-load reload
+  []
+  (reset! data (fetch-data))
   (refresh/refresh!))
+
+
+
+;;
+;; Suspense stuff
+;;
+
+
+
+(defprotocol IResource
+  (-read [args]))
+
+
+(defn read [resource & args]
+  (apply -read resource args))
+
+
+(defn wrap-promise
+  [p]
+  (let [status (atom :pending)
+        result (atom nil)
+        suspender (-> p
+                      (.then (fn [res]
+                               (reset! status :success)
+                               (reset! result res))
+                             (fn [err]
+                               (reset! status :failure)
+                               (reset! result err))))]
+    ;; Copying the contract from the codebox examples here:
+    ;; https://reactjs.org/docs/concurrent-mode-suspense.html
+    (reify
+      IResource
+      (-read [args]
+        (case @status
+           :pending (throw suspender)
+           :error (throw @result)
+           :success @result)))))
+
+
+(defn fetch-data []
+  (-> (js/Promise.
+       (fn fetch-data-promise [yes _no]
+         (prn :fetching)
+         (js/setTimeout #(do (prn :fetched)
+                             (yes {:result (rand-int 1000)}))
+                        4000)))
+      (wrap-promise)))
