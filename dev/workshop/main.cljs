05c216cebfcfa9be6d17cd553c4b98364f050eab
Will Acton <will@amperity.com>
2020-02-23 22:18:03 -0800

    move workshop to workshop.main

--- dev/workshop/main.cljs
+++ dev/workshop/main.cljs
@@ -1,8 +1,8 @@
-(ns workshop
+(ns ^:figwheel-hooks workshop.main
   (:require [devcards.core :as dc :include-macros true]
             [workshop.core]))
 
-(defn ^:dev/after-load start! []
+(defn ^:dev/after-load ^:after-load start! []
   (dc/start-devcard-ui!))
 
 (defn init! [] (start!))
