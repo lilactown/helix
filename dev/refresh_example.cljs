3a4560b8f286d869d49eaa11941029e3c3a61dc7
Will Acton <will@amperity.com>
2020-03-06 10:58:49 -0800

    update easy refresh-example

--- dev/refresh_example.cljs
+++ dev/refresh_example.cljs
@@ -1,24 +1,33 @@
 (ns refresh-example
-  (:require [helix.core :as hx :refer [<> defnc]]
-            [helix.dom :as d]
-            [helix.hooks :as hooks]
-            [refresh-example.depended :refer [greet]]
-            ["react-dom" :as rdom]))
+  (:require
+    [helix.core :refer [<> defnc $]]
+    [helix.dom :as d]
+    [helix.hooks :as hooks]
+    [helix.experimental.refresh :as refresh]
+    ["react-dom" :as rdom]))
 
 (defnc app
   []
+  {:helix/features {:fast-refresh true}}
   (let [[name set-name] (hooks/use-state "Lisa")]
     (d/div
      {:style {:text-align "center"
               :padding "10px"
-              :color "green"
+              :color "red"
               :font-family "sans-serif"}}
-     (d/div (greet name))
+     (d/div (str "hello, " name))
      (d/div
       (d/input {:value name :on-change #(set-name (.. % -target -value))})))))
 
 
-(defn ^:export start []
+(defn ^:dev/after-load reload
+  []
+  (refresh/refresh!))
+
+
+(defn ^:export start
+  []
+  (refresh/inject-hook!)
   (rdom/render
-   (app)
+   ($ app)
    (js/document.getElementById "app")))
