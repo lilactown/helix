5d767d830487fe461c09be44a6580f4870f1797f
Will Acton <will@willacton.com>
2020-11-21 12:47:27 -0800

    prepare for 0.0.14 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lilactown/helix "0.0.13"
+(defproject lilactown/helix "0.0.14-SNAPSHOT"
   :description "ClojureScript optimized for modern React development."
-  :url "https://github.com/Lokeh/helix"
+  :url "https://github.com/lilactown/helix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :source-paths ["src"]
