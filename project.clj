d903498e65de187632079601043a3ea51a73e509
Will Acton <will@willacton.com>
2021-09-18 15:24:09 -0700

    include resources in JAR

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/lilactown/helix"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
-  :source-paths ["src"]
+  :source-paths ["src" "resources"]
   :dependencies [[cljs-bean "1.5.0"]]
   :deploy-repositories [["snapshots" {:sign-releases false :url "https://clojars.org"}]])
