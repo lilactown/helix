fc2bfbfdd98a505561281376efdf0c620f8faef8
Rayat <rayat@amperity.com>
2022-05-08 23:20:41 -0700

    basic kondo hooks for $ and defnc

---
+++ dev/workshop/clj_kondo_hooks_test.cljs
@@ -0,0 +1,52 @@
+(ns workshop.clj-kondo-hooks-test
+  (:require ["react" :as react]
+            [helix.core :refer-macros [defnc] :refer [$]]
+            [helix.dom :as d]))
+
+
+(defn comp-printer [comp] (println (str comp)))
+
+(defnc my-comp-kitchen-sink
+  "optional docstring"
+  {:meta 'data}
+  [{:keys [children]} _ref]
+  {:helix/features :fast-refresh
+   :wrap           [(comp-printer) (react/forwardRef)]}
+  (d/div {:style {:display "flex"}} children))
+
+(defnc my-comp-no-doc
+  [{:keys [children]} _ref]
+  {:helix/features :fast-refresh
+   :wrap           [(comp-printer) (react/forwardRef)]}
+  ($ d/div {:style {:display "flex"}} children))
+
+(defnc my-comp-no-opts
+  "optional docstring"
+  [{:keys [children]} _ref]
+  ($ d/div {:style {:display "flex"}} children))
+
+(defnc my-comp-no-wrap-in-opts
+  "optional docstring"
+  [{:keys [children]} _ref]
+  {:helix/features :fast-refresh}
+  ($ d/div {:style {:display "flex"}} children))
+
+(defnc my-comp
+  [{:keys [children]} _ref]
+  (d/div   children))
+
+
+(d/div ($ my-comp
+          {:some :prop
+           &     {:foo :bar}}
+
+          ($ my-comp-kitchen-sink "ba1")
+          ($ my-comp-no-doc "baa")
+          ($ my-comp-no-opts "bab")
+          ($ my-comp-no-wrap-in-opts "bac")
+          "baz"
+          #_{:bad :map-children}))
+
+($ my-comp {:some :prop
+            &     {:foo :bar}}
+   #_{:bad :map-children})
---
+++ resources/clj-kondo.exports/lilactown/helix/clj_kondo/lilactown/helix.clj
@@ -0,0 +1,68 @@
+(ns clj-kondo.lilactown.helix
+  (:require [clj-kondo.hooks-api :as api]))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn $
+  "Macro analysis for `helix.core/$`."
+  [{:keys [node]}]
+  (let [[dollar-sym component-sym & body] (-> node :children)
+
+        old-props                         (-> body first :children)
+        children                          (-> body rest)
+        new-props                         (->> old-props
+                                               (map #(if (cond-> (api/sexpr %)
+                                                           symbol? (= '&))
+                                                       (api/keyword-node :&)
+                                                       %)))
+
+        expanded                          (api/list-node
+                                           (list*
+                                            dollar-sym
+                                            component-sym
+                                            (api/map-node new-props)
+                                            children))]
+
+    (when (->> children (some api/map-node?))
+      (api/reg-finding! (doto (merge
+                               {:message "Map passed as children"
+                                :type    :helix/invalid-children}
+                               (meta children)))))
+
+    {:node expanded}))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn defnc
+  "Macro analysis for `helix.core/defnc`."
+  [{:keys [node]}]
+  (let [[_ component-name & body] (-> node :children)
+        render-children           (last body)
+        the-rest                  (butlast body)
+        [docstring the-rest]      (if (api/string-node? (first the-rest))
+                                    [(first the-rest) (rest the-rest)]
+                                    [nil the-rest])
+        [metadata-map the-rest]   (if (api/map-node? (first the-rest))
+                                    [(first the-rest) (rest the-rest)]
+                                    [nil the-rest])
+        [argvec the-rest]         (if (api/vector-node? (first the-rest))
+                                    [(-> the-rest first) (rest the-rest)]
+                                    [nil the-rest])
+        opts-node                 (when (api/map-node? (first the-rest))
+                                    (first the-rest))
+        ;; wrap-opts                 (if opts-node  opts-node)
+        ;; new-wrap-opts             (if opts-node  wrap-opts)
+        new-opts                  (if opts-node (-> opts-node
+                                                    api/sexpr
+                                                    (assoc :wrap (api/sexpr (api/list-node (list* (api/token-node '->) (api/token-node '(helix.core/fnc [] "")) (-> opts-node api/sexpr :wrap api/coerce :children)))))
+                                                    api/coerce) opts-node)
+        expanded                  (api/list-node
+                                   (list* (api/token-node 'defn)
+                                          component-name
+                                          (filter some? [docstring
+                                                         metadata-map
+                                                         argvec
+                                                         new-opts
+                                                         render-children])))]
+    (comment #_do (prn)
+             (-> node api/sexpr prn)
+             (-> expanded api/sexpr prn))
+    {:node expanded}))
