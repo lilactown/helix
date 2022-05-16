5e5f74e193d3e89d959483b6fc11dd4fca15ac17
Will Acton <will@willacton.com>
2022-05-16 10:02:57 -0700

    closing tags, styles, handle deferreds, fragments

--- src/helix/server/dom.clj
+++ src/helix/server/dom.clj
@@ -2,6 +2,7 @@
   (:require
    [clojure.string :as string]
    [helix.server.core :as core]
+   [manifold.deferred :as d]
    [manifold.stream :as s])
   (:import [helix.server.core Element]))
 
@@ -21,20 +22,93 @@
   nil (to-str [_] ""))
 
 
+(def no-close-tag?
+  #{"area" "base" "br" "col" "command" "embed" "hr" "img" "input" "keygen" "link"
+    "meta" "param" "source" "track" "wbr"})
+
+
+(defn style-str
+  [styles]
+  (reduce-kv
+   (fn [s k v]
+     (str s (name k) ": " v ";"))
+   ""
+   styles))
+
+
+#_(style-str {:color "red"})
+
+
+(defn props->attrs
+  [props]
+  (reduce-kv
+   (fn [attrs k v]
+     (case k
+       :style (str attrs " style=\"" (style-str v) "\"")
+       attrs))
+   ""
+   props))
+
+
+#_(props->attrs {:style {:color "red"}})
+
+
 (defn put-el!
   [stream el]
-  (if (instance? Element el)
-    (if (string? (:type el))
-     (do (s/put! stream (str "<" (:type el) ">"))
-         (doseq [child (-> el :props :children)]
-           (put-el! stream child))
-         (s/put! stream (str "</" (:type el) ">")))
-     (put-el! stream (core/-render (:type el) (:props el))))
-    (s/put! stream (to-str el))))
+  (cond
+    (instance? Element el)
+    (cond
+      (no-close-tag? (:type el))
+      (s/put! stream (str "<" (:type el) (props->attrs (:props el)) " />"))
+
+      (string? (:type el))
+      (do (s/put! stream (str "<" (:type el) (props->attrs (:props el)) ">"))
+          (doseq [child (-> el :props :children)]
+            (put-el! stream child))
+          (s/put! stream (str "</" (:type el) ">")))
+
+      (= 'react/Fragment (:type el))
+      (doseq [child (-> el :props :children)]
+        (put-el! stream child))
+
+      :else (put-el! stream (core/-render (:type el) (:props el))))
 
 
-(let [stream (s/stream)]
-  (put-el! stream  ($d "div" ($d "span" "hello")))
+    (d/deferrable? el)
+    (put-el! stream @el)
+    #_(d/on-realized
+     el
+     #(put-el! stream %)
+     #(throw (ex-info "oh no" {:error %})))
+
+    (sequential? el)
+    (doseq [x el]
+      (put-el! stream x))
+
+    :else (s/put! stream (to-str el))))
+
+
+(defn page [{:keys [count] :or {count 10}}]
+  (let [color (get ["red" "green" "blue" "black"] (rand-int 3))]
+    ($d "html"
+        ($d "head"
+            ($d "title" "Streaming test"))
+        ($d "body"
+            {:style {:color color}}
+            ($d "div"
+                ($d "div"
+                    ($d "input"))
+                (for [i (range 0 count)]
+                ;; every 10 wait 100ms
+                  (if (zero? (mod i 10))
+                    (d/future
+                      (Thread/sleep 100)
+                      ($d "div" {:key i} "hello" i))
+                    ($d "div" {:key i} "hi" i))))))))
+
+
+#_(let [stream (s/stream)]
+  (put-el! stream (core/$ page))
   (s/close! stream)
   (s/stream->seq stream))
 
@@ -53,22 +127,17 @@
   (s/close! stream)
 
   (defn handler [req]
-    {:status 200
-     :headers {"content-type" "text/plain"}
-     :body stream})
-
-  (defn streaming-numbers-handler
-    [{:keys [params]}]
-    (let [cnt (Integer/parseInt (get params "count" "1000"))]
+    (let [stream (s/stream)]
+      (s/put! stream "<!doctype html>")
+      (d/future
+        (put-el! stream (core/$ page {:count 1000}))
+        (s/close! stream)
+        (prn "closed"))
       {:status 200
-       :headers {"content-type" "text/plain"}
-       :body (let [sent (atom 0)]
-               (->> (s/periodically 100 #(str (swap! sent inc) "\n"))
-                    (s/transform (take cnt))))}))
+       :headers {"content-type" "text/html"}
+       :body stream}))
 
-  (def server (http/start-server streaming-numbers-handler {:port 8080}))
-
-  (def server (http/start-server handler {:port 8080}))
+  (def server (http/start-server #'handler {:port 8080}))
 
   (.close server))
 
