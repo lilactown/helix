05eda8765d28670db280d035086388e6a1aefa6a
Will Acton <will@willacton.com>
2022-10-23 10:26:27 -0700

    add separate analysis from helix.dom/* to fix NPE

--- resources/clj-kondo.exports/lilactown/helix/clj_kondo/lilactown/helix.clj
+++ resources/clj-kondo.exports/lilactown/helix/clj_kondo/lilactown/helix.clj
@@ -4,12 +4,10 @@
 
 
 (defn $
-  "Macro analysis for `helix.core/$` & `helix.dom/*`."
+  "Macro analysis for `helix.core/$` & `helix.dom/$d`."
   [{:keys [node]}]
   (let [[fn-sym & body] (-> node :children)
-        [component-sym body] (if (api/token-node? (first body))
-                               [(first body) (next body)]
-                               [nil body])
+        [component-sym body] [(first body) (next body)]
         [old-props body] (if (api/map-node? (first body))
                            [(-> body first :children) (next body)]
                            [nil body])
@@ -23,6 +21,26 @@
                          api/map-node))
         expanded (api/list-node
                   (list* fn-sym component-sym new-props children))]
+    {:node (with-meta expanded (meta node))}))
+
+
+(defn dom
+  "Macro analysis for `helix.dom/*`."
+  [{:keys [node]}]
+  (let [[fn-sym & body] (-> node :children)
+        [old-props body] (if (api/map-node? (first body))
+                           [(-> body first :children) (next body)]
+                           [nil body])
+        children body
+        new-props (when old-props
+                    (->> old-props
+                         (map
+                          #(if (cond-> (api/sexpr %) symbol? (= '&))
+                             (api/keyword-node :&)
+                             %))
+                         api/map-node))
+        expanded (api/list-node
+                  (list* fn-sym new-props children))]
     {:node (with-meta expanded (meta node))}))
 
 
